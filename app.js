858db9ab33bb734ba505b3c18dbb2a653eeb7b67
Sofie Larsen <Sofieamalierl@hotmail.com>
2023-03-20 15:25:49 +0100

    to do list / delete knap

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 "use strict";
+
+window.addEventListener("load", initApp);
+
+const newToDo = document.querySelector("#input-new-to-do");
+const list = document.querySelector("#to-do-list");
+
+function initApp() {
+    document.querySelector("#btn-add-item").addEventListener("click", addToDo);
+}
+
+function addToDo() {
+    const listItem = document.createElement("li");
+    const listText = document.createElement("span");
+    const listBtn = document.createElement("button");
+
+    listItem.textContent = newToDo.value;
+    listBtn.textContent = "delete";
+
+    listItem.appendChild(listText);
+    listItem.appendChild(listBtn);
+
+    list.appendChild(listItem);
+
+    newToDo.value = "";
+
+   listBtn.addEventListener("click", removeToDo);
+}
+function removeToDo() {
+    this.parentNode.remove();
+}
+
